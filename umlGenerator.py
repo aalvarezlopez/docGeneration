ac77956a15d0f855f8ca788baf770b390dde2db5
Adrian <alvarez.lopez.adri@gmail.com>
2017-10-03 22:31:09 +0100

    Simple class diagram draw

--- umlGenerator.py
+++ umlGenerator.py
@@ -47,12 +47,14 @@ class umlGenerator():
         for f in fctags:
             if self.swcName+'.h' in f and len(f.split('\t')) > 3:
                 if f.split('\t')[3] == 'p':
-                    self.privateMethods.append(f.split('\t')[0])
+                    self.publicMethods.append(f.split('\t')[0])
         fctags.seek(0)
         for f in fctags:
-            if self.swcName+'.c' in f and len(f.split('\t')) > 3:
-                if f.split('\t')[3] == 'f' and f.split('\t')[0] not in self.privateMethods:
-                    self.publicMethods.append(f.split('\t')[0])
+            if self.swcName+'.c' in f and len(f.split('\t')) >= 3:
+                if f.split('\t')[3].replace('\n','') == 'f' and f.split('\t')[0] not in self.publicMethods:
+                    self.privateMethods.append(f.split('\t')[0])
+                if f.split('\t')[3].replace('\n','') == 'v':
+                    self.privateAttributes.append(f.split('\t')[0])
 
         fctags.close()
 
@@ -65,14 +67,31 @@ class umlGenerator():
         fin = open('./classTemplate.uxf', 'r')
         fout = open('./%sClassDgm.uxf'%self.swcName, 'w')
         for line in fin:
-            fout.write(line)
+            if "#ATTRIBUTES#" in line:
+                for method in self.publicAttributes:
+                    fout.write("+ %s\n"%method)
+                for method in self.privateAttributes:
+                    fout.write("- %s\n"%method)
+            elif "#METHODS#" in line:
+                for method in self.publicMethods:
+                    fout.write("+ %s\n"%method)
+                for method in self.privateMethods:
+                    fout.write("- %s\n"%method)
+            elif "#NAME#" in line:
+                fout.write(line.replace("#NAME#", self.swcName))
+            elif "<h>" in line:
+                fout.write("<h>%d</h>"%((4\
+                        + len(self.privateMethods)\
+                        + len(self.privateAttributes)\
+                        + len(self.publicMethods)\
+                        + len(self.publicAttributes)) * 15))
+            else:
+                fout.write(line)
 
         fout.write('#Date:%d %d %d - GIT release: %s'%(3, 10, 2017, 'r01ase'))
 
         fin.close()
         fout.close()
-        print self.privateMethods
-        print self.publicMethods
 
 
 
